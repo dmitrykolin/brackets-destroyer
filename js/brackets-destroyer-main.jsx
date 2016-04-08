dfc405cfd0f7343c0dba1723e612dd54d10217e1
Dmitry Kolin <kolin.dmitry@gmail.com>
2016-04-08 20:35:45 +0300

    removed log

--- js/brackets-destroyer-main.jsx
+++ js/brackets-destroyer-main.jsx
@@ -6,7 +6,6 @@ import bracketsDestroyerService from './brackets-destroyer-service.js';
 var bdService = bracketsDestroyerService.service;
 var defaultExpression = "5 * (1 + sin(2))";
 bdService.initialize(defaultExpression);
-console.log(1);
 
 ReactDOM.render(
     <BracketsDestroyerBox bracketsDestroyerService={bdService}/>,
